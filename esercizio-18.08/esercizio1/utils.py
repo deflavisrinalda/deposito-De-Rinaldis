a223d477590489666c85a24dc8b7457297185a32
deflavisrinalda <Flavia.De.Rinaldis@it.ey.com>
2025-08-18 12:24:15 +0200

    feat: aggiunto funzionalità del conteggio delle top 5 parole
    aggiunto def top_five per restituire le top 5 parole presenti nel txt

--- esercizio-18.08/esercizio1/utils.py
+++ esercizio-18.08/esercizio1/utils.py
@@ -23,3 +23,23 @@ def total_words(file_path: str) -> int:
     return len(words)
 
 print(total_words("prova.txt"))
+
+def top_five(file_path: str) -> list:
+    
+    opened_file = open_file(file_path)
+    words = opened_file.split()
+    word_count = {}
+    
+    for word in words:
+        word = word.lower().strip('.,!?";:')
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+            
+    sorted_words = sorted(word_count.items(), key=lambda item: item[1], reverse=True)
+    top_five_words = [word for word, count in sorted_words[:5]]
+    
+    return top_five_words
+
+print(top_five("prova.txt"))
