76725558437324d25799acf4f26d2fd044c26e87
deflavisrinalda <Flavia.De.Rinaldis@it.ey.com>
2025-08-18 12:03:59 +0200

    feat: creato la struttura iniziale del progetto
    aggiunto main.py
    aggiunto utils.py dove aggiungere le funzionalità
    aggiunto prova.txt per l'input

---
+++ esercizio-18.08/esercizio1/main.py
@@ -0,0 +1,15 @@
+
+
+def main():
+
+    #numero totale righe
+
+    #numero totale parole
+
+    #top5 parole più frequenti
+    print("ciao")
+
+if __name__ == "__main__":
+    main()
+    # This is a simple Python script that prints a greeting message.
+    # It serves as an entry point for the program.
