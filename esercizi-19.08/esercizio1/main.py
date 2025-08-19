cca094b1bc2ae5d91b73649849cf734fa371836c
deflavisrinalda <Flavia.De.Rinaldis@it.ey.com>
2025-08-19 10:59:46 +0200

    feat: creazione classificazione rispetto alla media giornaliera
    aggiunta nel df la colonna relativa alla classificazione giornaliera

--- esercizi-19.08/esercizio1/main.py
+++ esercizi-19.08/esercizio1/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 #estraggo il file
 file_path = './DUQ_hourly.csv'
-df = pd.read_csv(file_path, parse_dates=['Datetime'])
+df = pd.read_csv(file_path, parse_dates=['Datetime'], index_col='Datetime')
 
 #controllo delle prime e ultime righe
 print(df.head())
@@ -15,5 +15,13 @@ media_globale = df[colonna_consumo].mean()
 df["classificazione_globale"] = df[colonna_consumo].apply(lambda x: "sotto media" if x < media_globale else "sopra media")
 
 #check creazione classificazione globale
+print(df.head())
+print(df.tail())
+
+#classificazione rispetto alla media giornaliera
+media_giornaliera = df.groupby(df.index.date)[colonna_consumo].mean().mean()
+df["classificazione_giornaliera"] = df[colonna_consumo].apply(lambda x: "sotto media" if x < media_giornaliera else "sopra media")
+
+#check creazione classificazione giornaliera
 print(df.head())
 print(df.tail())
