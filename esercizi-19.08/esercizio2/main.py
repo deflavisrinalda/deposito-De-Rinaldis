cd82e99462b65c993ad9a5c34871d8b403357a23
deflavisrinalda <Flavia.De.Rinaldis@it.ey.com>
2025-08-19 16:01:39 +0200

    feat: creazione colonna datetime

--- esercizi-19.08/esercizio2/main.py
+++ esercizi-19.08/esercizio2/main.py
@@ -1,6 +1,25 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('./AirQualityUCI.csv', sep=';')
 
 print(df.head())
 print(df.tail())
+print(df.shape[0])
+
+#rimuovo colonne unnamed
+df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+
+print(df.head())
+print(df.tail())
+print(df.shape[0])
+
+#creo la colonna datetime
+df["datetime"] = pd.to_datetime(df["Date"] + " " + df["Time"], format= "%d/%m/%Y %H.%M.%S", errors='coerce' )
+df = df.dropna(subset=["datetime"])
+df.set_index("datetime", inplace=True)
+
+#check
+print(df.head())
+print(df.tail())
+print(df.shape[0])
