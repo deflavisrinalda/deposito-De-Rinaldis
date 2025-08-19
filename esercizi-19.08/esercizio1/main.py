75c074249b8ae2f9fd6733deb9a2e41fe70811cc
deflavisrinalda <Flavia.De.Rinaldis@it.ey.com>
2025-08-19 10:54:24 +0200

    feat: creazione classificazione globale
    creazione della colonna classificazione globale nel df

--- esercizi-19.08/esercizio1/main.py
+++ esercizi-19.08/esercizio1/main.py
@@ -1,8 +1,19 @@
 import pandas as pd
 
+#estraggo il file
 file_path = './DUQ_hourly.csv'
-df = pd.read_csv(file_path)
+df = pd.read_csv(file_path, parse_dates=['Datetime'])
 
+#controllo delle prime e ultime righe
 print(df.head())
+print(df.tail())
 
+colonna_consumo = "DUQ_MW"
+
+#classidicazione rispetto alla media globale
+media_globale = df[colonna_consumo].mean()
+df["classificazione_globale"] = df[colonna_consumo].apply(lambda x: "sotto media" if x < media_globale else "sopra media")
+
+#check creazione classificazione globale
+print(df.head())
 print(df.tail())
