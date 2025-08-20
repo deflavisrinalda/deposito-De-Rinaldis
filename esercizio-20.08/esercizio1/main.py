a90581e9ac70fc9111703c37ec96d4d650631f7d
deflavisrinalda <Flavia.De.Rinaldis@it.ey.com>
2025-08-20 11:28:00 +0200

    fix: cambio di dati in dati standardizzati

--- esercizio-20.08/esercizio1/main.py
+++ esercizio-20.08/esercizio1/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv('Mall_Customers.csv')
 
@@ -8,18 +9,21 @@ df = pd.read_csv('Mall_Customers.csv')
 print(df.head())
 print(df.tail())
 print(df.info())
-print(df[df['CustomerID'] == 1])
+#print(df[df['CustomerID'] == 1])
 
 # Selezione feature da utilizzare
 X = df[['Annual Income (k$)', 'Spending Score (1-100)']]
+# feature standardizzate
+X_scaled = StandardScaler().fit_transform(X)
 
 # Applico il clustering K-Means
 kmeans = KMeans(n_clusters=5, random_state=42)
-df['Cluster'] = kmeans.fit_predict(X)
+df['Cluster'] = kmeans.fit_predict(X_scaled)
 
 # Visualizzazione dei risultati
-plt.scatter(X['Annual Income (k$)'], X['Spending Score (1-100)'], c=df['Cluster'], cmap='viridis')
+plt.scatter(X_scaled[:, 0], X_scaled[:, 1], c=df['Cluster'], cmap='viridis')
 plt.xlabel('Annual Income (k$)')
 plt.ylabel('Spending Score (1-100)')
 plt.title('Clustering dei clienti')
 plt.show()
+
