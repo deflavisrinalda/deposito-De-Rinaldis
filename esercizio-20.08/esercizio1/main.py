d27ac1fb7038f55b772c5d2a1ba0cccb13664087
deflavisrinalda <Flavia.De.Rinaldis@it.ey.com>
2025-08-20 11:20:17 +0200

    feat: K-Means su le colonne annual income e spending score SENZA standardizzazione

--- esercizio-20.08/esercizio1/main.py
+++ esercizio-20.08/esercizio1/main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('Mall_Customers.csv')
 
@@ -7,3 +9,17 @@ print(df.head())
 print(df.tail())
 print(df.info())
 print(df[df['CustomerID'] == 1])
+
+# Selezione feature da utilizzare
+X = df[['Annual Income (k$)', 'Spending Score (1-100)']]
+
+# Applico il clustering K-Means
+kmeans = KMeans(n_clusters=5, random_state=42)
+df['Cluster'] = kmeans.fit_predict(X)
+
+# Visualizzazione dei risultati
+plt.scatter(X['Annual Income (k$)'], X['Spending Score (1-100)'], c=df['Cluster'], cmap='viridis')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.title('Clustering dei clienti')
+plt.show()
