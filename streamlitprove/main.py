87ceab7792af44a61f08c8f9255f7d9eaa8ebb6c
deflavisrinalda <Flavia.De.Rinaldis@it.ey.com>
2025-08-22 14:40:25 +0200

    prove_streamlit

--- streamlitprove/main.py
+++ streamlitprove/main.py
@@ -1,6 +1,29 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+
+st.title('Counter Example')
+
+# #esempio NON funzionante
+# count = 0
+
+# increment = st.button('Increment')
+# if increment:
+#     count += 1
+
+# st.write('Count = ', count)
+
+#esempio funzionante
+# Inizializza il contatore solo una volta
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+# Bottone per incrementare
+if st.button('Increment'):
+    st.session_state.count += 1
+
+# Mostra il valore attuale
+st.write('Count = ', st.session_state.count)
 
 
-st.title("App Streamlit di esempio")
+# Esempio bottone di reset
+if st.button('Reset'):
+    st.session_state.count = 0
